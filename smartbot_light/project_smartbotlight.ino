46d41ca51dca745ccd1b124764f2652a1fdee36b
be4rz <chantran1111@gmail.com>
2024-04-19 10:49:36 +0900

    set buffer size

--- smartbot_light/project_smartbotlight.ino
+++ smartbot_light/project_smartbotlight.ino
@@ -1,19 +1,26 @@
 #include "string.h"
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#include <ArduinoJson.h>
+#include <Arduino_JSON.h>
+#include "DHT.h"
 
 // WiFi
-const char *ssid = "Isling iPhone"; // Enter your WiFi name
-const char *password = "tumotden8";  // Enter WiFi password
+const char *ssid = "AnDK"; // Enter your WiFi name
+const char *password = "123456789";  // Enter WiFi password
 
 //device config
-const char *deviceId = "01HTQ9844VMWWP70AD8HGJ48BY";
+const char *deviceId = "01HVAJ1N63VW5BAA6SDDWNB88Q";
 char spaceId[30] = "01HTQ9HBANAVTXG95AQWSWGXKK";
 
 // MQTT Broker
+#define MQTT_MAX_PACKET_SIZE 512
+#define MQTT_HOST "mqtt.dev.isling.me"
+#define MQTT_PORT 1883
+#define MQTT_PASS ""
+#define DEVICE_ID "01HVAJ1N63VW5BAA6SDDWNB88Q"
+
 const char *mqtt_broker = MQTT_HOST;
-const char *cmdTopic = "light_sensor";
+char *cmdTopic;
 const char *mqtt_username = deviceId;
 const char *mqtt_password = MQTT_PASS;
 const int mqtt_port = MQTT_PORT;
@@ -22,15 +29,19 @@ const char *pingTopic = "ping/iot_devices";
 // Analog pin the photoresistor is connected to
 const int photoResistorPin = A0;
 unsigned long previousMillis = 0;
+const int DHTPIN = 2;
+#define DHTTYPE DHT11 // DHT 11
+
+DHT dht(DHTPIN, DHTTYPE);
 
 WiFiClient espClient;
-PubSubClient client(espClient);
+PubSubClient mqttClient(espClient);
 
 void connectToWiFi();
 void connectToMQTTBroker();
-void mqttCallback(char *topic, byte *payload, unsigned int length);
 
 void setup() {
+
     // Set software serial baud to 115200;
     Serial.begin(115200);
     
@@ -41,12 +52,14 @@ void setup() {
     cmdTopic = (char *)malloc(cmdTopicS.length() + 1);
     strcpy(cmdTopic, cmdTopicS.c_str());
 
+    //Set the maximum MQTT packet size
+    mqttClient.setBufferSize(MQTT_MAX_PACKET_SIZE);
     //connecting to a mqtt broker
-    client.setServer(mqtt_broker, mqtt_port);
-    client.setCallback(mqttCallback);
+    mqttClient.setServer(mqtt_broker, mqtt_port);
     connectToMQTTBroker();
-}
 
+    dht.begin();
+}
 
 void loop() {
     if (WiFi.status() != WL_CONNECTED) {
@@ -64,18 +77,66 @@ void loop() {
     if (currentMillis - previousMillis >= 5000) {
         previousMillis = currentMillis;
         int lightValue = analogRead(photoResistorPin);
-        char *lightStatus = lightValue > 100 ? "night" : "day";
+        float temp = dht.readTemperature();
+        float hum = dht.readHumidity();
 
-        // json serialize
-        DynamicJsonDocument data(256);
-        data["value"] = lightValue;
-        data["status"] = lightStatus;
-        // publish temperature and humidity
-        char json_string[256];
-        serializeJson(data, json_string);
+        // // json serialize
+        // DynamicJsonDocument data(512);
+        // data["iotDeviceId"]=deviceId;
+        // data["timestamp"]="4/13/2024 12:22:00";
+        
+        // JsonArray measurementsArray  = data.createNestedArray("measurements");
+
+        // //Add temperature measurement
+        // JsonObject temperatureMeasurement = measurementsArray.createNestedObject();
+        // temperatureMeasurement["type"] = "temperature";
+        // temperatureMeasurement["value"] = temp;
+
+        // // Add humidity measurement
+        // JsonObject humidityMeasurement = measurementsArray.createNestedObject();
+        // humidityMeasurement["type"] = "humidity";
+        // humidityMeasurement["value"] = hum;
+
+        // // Add light measurement
+        // JsonObject lightMeasurement = measurementsArray.createNestedObject();
+        // lightMeasurement["type"] = "light";
+        // lightMeasurement["value"] = lightValue;
+
+        // // publish temperature and humidity
+        // char json_string[512];
+        // serializeJson(data, json_string);
+
+        // Clear previous measurements
+
+        JSONVar measurements;
+
+        // Add measurements to the JSONVar object
+        measurements["iotDeviceId"] = deviceId;
+        measurements["timestamp"] = "2024-04-08T02:38:00Z";
+
+        JSONVar measurementsArray;
+        JSONVar temperatureMeasurement;
+        temperatureMeasurement["type"] = "temperature";
+        temperatureMeasurement["value"] = temp;
+        measurementsArray[0]=temperatureMeasurement;
+
+        JSONVar humidityMeasurement;
+        humidityMeasurement["type"] = "humidity";
+        humidityMeasurement["value"] = hum;
+        measurementsArray[1]=humidityMeasurement;
+
+        JSONVar lightMeasurement;
+        lightMeasurement["type"] = "light";
+        lightMeasurement["value"] = lightValue;
+        measurementsArray[2]=lightMeasurement;
+
+        measurements["measurements"] = measurementsArray;
+
+        // Serialize the JSONVar object
+        String json_string = JSON.stringify(measurements);
 
         Serial.println(json_string);
-        client.publish(cmdTopic, json_string, false);
+        boolean result = mqttClient.publish(cmdTopic, json_string.c_str());
     }
 }
 
@@ -96,7 +157,7 @@ void connectToMQTTBroker() {
       if (mqttClient.connect(clientId.c_str(), mqtt_username, mqtt_password)) {
           Serial.println("Connected to MQTT broker");
           mqttClient.subscribe(cmdTopic);
-          Serial.printf("Subscribe topic %s\n", topic);
+          Serial.printf("Subscribe topic %s\n", cmdTopic);
           // Publish message upon successful connection
           mqttClient.publish(pingTopic, deviceId);
       } else {
